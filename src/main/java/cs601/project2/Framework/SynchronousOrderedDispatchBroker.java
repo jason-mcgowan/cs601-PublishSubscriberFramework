321c77799e732b67194e2fae189721ee89d04812
Jason McGowan <jmcgowan3@dons.usfca.edu>
2021-09-23 16:32:54 -0700

    Removed the thread pool experiment from SynchronousOrderedDispatchBroker

--- src/main/java/cs601/project2/Framework/SynchronousOrderedDispatchBroker.java
+++ src/main/java/cs601/project2/Framework/SynchronousOrderedDispatchBroker.java
@@ -1,39 +1,31 @@
 package cs601.project2.Framework;
 
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
+/*
+Design notes:
+1. Tried with synchronizing on the subscriber list.
+2. Moved to using a read lock on subscribers, runs faster (about 4 seconds in test case).
+3. Tried using a thread pool to process each publish to the subscribers, was exceedingly slow.
+Would only be useful in the case where you had subscribers with downtime.
+ */
 
+/**
+ * Blocks on any publish call until all subscribers complete processing.
+ */
 public class SynchronousOrderedDispatchBroker<T> extends AbstractBroker<T> {
-
-  private final ExecutorService pool = Executors.newFixedThreadPool(
-      Runtime.getRuntime().availableProcessors() * 2);
 
   @Override
   protected synchronized void publishNewItem(T item) {
     // Read lock so the subscriber list can't change while iterating
     subscriberLock.readLock().lock();
-    List<Callable<T>> tasks;
     try {
-      tasks = subscribers.stream()
-          .map(sub -> (Callable<T>) () -> {
-            sub.onEvent(item);
-            return null;
-          }).collect(Collectors.toList());
+      subscribers.forEach(sub -> sub.onEvent(item));
     } finally {
       subscriberLock.readLock().unlock();
-    }
-    try {
-      pool.invokeAll(tasks);
-    } catch (InterruptedException e) {
-      // Doesn't need to be handled
     }
   }
 
   @Override
   protected void publishRemainingBeforeShutdown() {
-    pool.shutdown();
+    // Nothing extra to do here
   }
 }
