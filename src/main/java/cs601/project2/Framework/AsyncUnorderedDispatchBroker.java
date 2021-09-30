184db94708e18786ff1d974a0a051d108d20d5e1
Jason McGowan <jmcgowan3@dons.usfca.edu>
2021-09-29 17:47:49 -0700

    AsyncUnorderedDispatchBroker changed to work stealing pool

--- src/main/java/cs601/project2/Framework/AsyncUnorderedDispatchBroker.java
+++ src/main/java/cs601/project2/Framework/AsyncUnorderedDispatchBroker.java
@@ -1,9 +1,11 @@
 /*
 Design notes:
+-Tried cached pool, was much slower than fixed
 -Tried different fixed thread pool sizes. CPU count * 2 was slow.
 -Tried adding each onEvent as individual tasks to the pool, was slower.
 -Fastest was with each task being to publish to all subscribers
 -This was verified with dummy time-consuming subscribers, too
+-Work stealing pool seems fastest, at least on my machine.
  */
 
 package cs601.project2.Framework;
@@ -22,8 +24,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class AsyncUnorderedDispatchBroker<T> extends AbstractBroker<T> {
 
-  protected final ExecutorService pool = Executors.newFixedThreadPool(
-      Runtime.getRuntime().availableProcessors() + 1);
+  protected final ExecutorService pool = Executors.newWorkStealingPool();
 
   @Override
   protected void publishNewItem(T item) {
